53de9b61298fa9712deb65c6528ce24a1f452088
juliap <juliap@ciandt.com>
2021-04-09 15:56:21 -0300

    add switch

--- src/components/FormularioCadastro/FormCadastro.jsx
+++ src/components/FormularioCadastro/FormCadastro.jsx
@@ -1,14 +1,31 @@
+import { Typography } from '@material-ui/core';
 import React, {useState} from 'react';
+import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 
 function FormCadastro({aoEnviar, validarCPF}) {
+    const [etapaAtual, setEtapaAtual] = useState(1);
+
+    function formAtual(etapa) {
+        switch(etapa){
+            case 0:
+                return <DadosUsuario/>;
+            
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>;
     
+            case 2:
+                return <DadosEntrega/>;
+            
+            default: 
+                return <Typography>Erro ao selecionar formulário</Typography>;
+        }
+    }
 
     return(
         <>
-        <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-        <DadosUsuario/>
+        {formAtual(etapaAtual)}
         </>
     )
     
